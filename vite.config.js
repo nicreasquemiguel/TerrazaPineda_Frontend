522561fc2531438bd91306460809b0c584a54960
nicreasquemiguel <miguelva703@gmail.com>
2025-09-29 00:11:21 -0600

    notificaiton 3

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
     assetsInclude: ['**/*.HEIC'],
   plugins: [vue()],
+  define: {
+    global: 'globalThis',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -13,6 +16,7 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+    host: true, // Allow external connections
     allowedHosts: [
       'terrazapineda.com',
       'www.terrazapineda.com'
